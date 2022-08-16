cf69b9024d6e223b05e38381ccd0d74a202aede7
vpasias <52623837+vpasias@users.noreply.github.com>
2022-08-16 18:32:57 +0300

    Update kubevirt.sh

--- kubevirt.sh
+++ kubevirt.sh
@@ -10,7 +10,7 @@ sleep 60
 sudo kubectl get pods -n kubevirt
 # Create the KubeVirt CR (instance deployment request) which triggers the actual installation
 sudo kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-cr.yaml
-sleep 90
+sleep 180
 sudo kubectl get pods -n kubevirt
 curl -L -o virtctl https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/virtctl-${KUBEVIRT_VERSION}-linux-amd64
 chmod +x virtctl
