1b7a507b54ccb2619741df669166197af2fe1bfb
vpasias <52623837+vpasias@users.noreply.github.com>
2022-08-16 15:31:02 +0300

    Update mon.sh

--- mon.sh
+++ mon.sh
@@ -3,7 +3,7 @@
 
 git clone https://github.com/prometheus-operator/kube-prometheus.git
 
-sudo kubectl create -f /home/iason/k8s_cluster/kube-prometheus/manifests/setup/
+sudo kubectl create -f /home/iason/k8s_cluster/kube-prometheus/manifests/setup
 
 sudo kubectl get ns monitoring
 
