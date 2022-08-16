aa7fac23a6a1aaaa45512c7dd5d3b8aa96d844e3
vpasias <52623837+vpasias@users.noreply.github.com>
2022-08-16 10:21:31 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -81,9 +81,11 @@ for i in {1..6}; do ssh -o StrictHostKeyChecking=no rocky@node-$i "sudo modprobe
 for i in {1..6}; do ssh -o StrictHostKeyChecking=no rocky@node-$i "sudo modprobe br_netfilter"; done
 
 for i in {1..6}; do ssh -o StrictHostKeyChecking=no rocky@node-$i "cat << EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_nonlocal_bind = 1
+net.ipv4.ip_forward = 1
+vm.swappiness=0
 EOF"; done
 
 for i in {1..6}; do ssh -o StrictHostKeyChecking=no rocky@node-$i "sudo sysctl --system"; done
