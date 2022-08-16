3447310c9bacdec22b42a898403c6ba8a493f650
vpasias <52623837+vpasias@users.noreply.github.com>
2022-08-16 15:44:42 +0300

    Update mon.sh

--- mon.sh
+++ mon.sh
@@ -3,8 +3,6 @@
 
 git clone https://github.com/prometheus-operator/kube-prometheus.git
 
-sudo kubectl create -f /home/iason/k8s_cluster/kube-prometheus/manifests/setup
-
 sudo kubectl apply --server-side -f /home/iason/k8s_cluster/kube-prometheus/manifests/setup
 
 until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
