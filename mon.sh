7b06353fbe90d5500011eb4385fc2e7fac79fe4b
vpasias <52623837+vpasias@users.noreply.github.com>
2022-08-16 15:07:39 +0300

    Update mon.sh

--- mon.sh
+++ mon.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+#
 
+git clone https://github.com/prometheus-operator/kube-prometheus.git
+
+sudo kubectl create -f /home/iason/k8s_cluster/kube-prometheus/manifests/setup
+
+sudo kubectl get ns monitoring
+
+sudo kubectl get pods -n monitoring
+
+sleep 120
+
+sudo kubectl create -f /home/iason/k8s_cluster/kube-prometheus/manifests/
+
+sleep 20
+
+sudo kubectl get pods -n monitoring
+
+sudo kubectl get svc -n monitoring
