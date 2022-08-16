ea1dfe004e734cb46add6701145551264f8df9e7
vpasias <52623837+vpasias@users.noreply.github.com>
2022-08-16 14:05:50 +0300

    Update rook.sh

--- rook.sh
+++ rook.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #
 
-git clone --single-branch --branch release-1.9 https://github.com/rook/rook.git && cd rook/deploy/examples/ 
+git clone --single-branch --branch release-1.9 https://github.com/rook/rook.git
+cd /home/iason/k8s_cluster/rook/deploy/examples/ 
 kubectl create -f crds.yaml
 sleep 5
 kubectl create -f common.yaml
 kubectl create -f operator.yaml
 kubectl get all -n rook-ceph
-sleep 60
+sleep 120
 kubectl -n rook-ceph get pod
 kubectl config set-context --current --namespace rook-ceph
+kubectl create -f /home/iason/k8s_cluster/cluster.yaml
