171380f4c5a5e04f30f2ce329be644be8884a4a5
vpasias <52623837+vpasias@users.noreply.github.com>
2022-08-17 10:19:07 +0300

    Update mon.sh

--- mon.sh
+++ mon.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 #
 
-git clone https://github.com/prometheus-operator/kube-prometheus.git
+sudo kubectl create namespace monitoring
 
-sudo kubectl apply --server-side -f /home/iason/k8s_cluster/kube-prometheus/manifests/setup
+# Add prometheus-community repo
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+# Update helm repo
+helm repo update
+
+helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring
+
+#git clone https://github.com/prometheus-operator/kube-prometheus.git
+#sudo kubectl apply --server-side -f /home/iason/k8s_cluster/kube-prometheus/manifests/setup
+#sleep 120
+#until sudo kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+#sudo kubectl apply -f /home/iason/k8s_cluster/kube-prometheus/manifests/
 
 sleep 120
-
-until sudo kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
-
-sudo kubectl apply -f /home/iason/k8s_cluster/kube-prometheus/manifests/
-
-sleep 20
 
 sudo kubectl get svc -n monitoring
 
